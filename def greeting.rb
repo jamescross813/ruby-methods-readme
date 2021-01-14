3c4676bef6ee52a6c727ce223af4b64822f06ff3
jamescross <crossjames813@gmail.com>
2021-01-13 22:55:02 -0600

    commit tree 66d25cb93593c71c071c39d6999b3db7ecc3602e

--- def greeting.rb
+++ def greeting.rb
@@ -1,5 +1,19 @@
-def greeting
-  puts "Hello World"
-end
-
-greeting 
+def say_hello_world_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+  end
+  
+  say_hello_world_ten_times
+  
+  # ... The rest of the program
+  
+  say_hello_world_ten_times 
